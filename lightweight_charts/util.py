47125149761d1650cd31c1c6942031eab822f614
tphlru <t.tunkin@yandex.ru>
2025-07-03 11:08:18 +0300

    add run_script_and_get

--- lightweight_charts/util.py
+++ lightweight_charts/util.py
@@ -10,36 +10,41 @@ import pandas as pd
 class Pane:
     def __init__(self, window):
         from lightweight_charts import Window
+
         self.win: Window = window
         self.run_script = window.run_script
+        self.run_script_and_get = window.run_script_and_get
         self.bulk_run = window.bulk_run
-        if hasattr(self, 'id'):
+        if hasattr(self, "id"):
             return
         self.id = Window._id_gen.generate()
 
 
 class IDGen(list):
-    ascii = 'abcdefghijklmnopqrstuvwxyz'
+    ascii = "abcdefghijklmnopqrstuvwxyz"
 
     def generate(self) -> str:
-        var = ''.join(choices(self.ascii, k=8))
+        var = "".join(choices(self.ascii, k=8))
         if var not in self:
             self.append(var)
-            return f'window.{var}'
+            return f"window.{var}"
         self.generate()
 
 
 def parse_event_message(window, string):
-    name, args = string.split('_~_')
-    args = args.split(';;;')
+    name, args = string.split("_~_")
+    args = args.split(";;;")
     func = window.handlers[name]
     return func, args
 
 
 def js_data(data: Union[pd.DataFrame, pd.Series]):
     if isinstance(data, pd.DataFrame):
-        d = data.to_dict(orient='records')
-        filtered_records = [{k: v for k, v in record.items() if v is not None and not pd.isna(v)} for record in d]
+        d = data.to_dict(orient="records")
+        filtered_records = [
+            {k: v for k, v in record.items() if v is not None and not pd.isna(v)}
+            for record in d
+        ]
     else:
         d = data.to_dict()
         filtered_records = {k: v for k, v in d.items()}
@@ -47,38 +52,40 @@ def js_data(data: Union[pd.DataFrame, pd.Series]):
 
 
 def snake_to_camel(s: str):
-    components = s.split('_')
-    return components[0] + ''.join(x.title() for x in components[1:])
+    components = s.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
+
 
 def js_json(d: dict):
     filtered_dict = {}
     for key, val in d.items():
-        if key in ('self') or val in (None,):
+        if key in ("self") or val in (None,):
             continue
-        if '_' in key:
+        if "_" in key:
             key = snake_to_camel(key)
         filtered_dict[key] = val
     return f"JSON.parse('{json.dumps(filtered_dict)}')"
 
 
-def jbool(b: bool): return 'true' if b is True else 'false' if b is False else None
+def jbool(b: bool):
+    return "true" if b is True else "false" if b is False else None
 
 
-LINE_STYLE = Literal['solid', 'dotted', 'dashed', 'large_dashed', 'sparse_dotted']
+LINE_STYLE = Literal["solid", "dotted", "dashed", "large_dashed", "sparse_dotted"]
 
-MARKER_POSITION = Literal['above', 'below', 'inside']
+MARKER_POSITION = Literal["above", "below", "inside"]
 
-MARKER_SHAPE = Literal['arrow_up', 'arrow_down', 'circle', 'square']
+MARKER_SHAPE = Literal["arrow_up", "arrow_down", "circle", "square"]
 
-CROSSHAIR_MODE = Literal['normal', 'magnet', 'hidden']
+CROSSHAIR_MODE = Literal["normal", "magnet", "hidden"]
 
-PRICE_SCALE_MODE = Literal['normal', 'logarithmic', 'percentage', 'index100']
+PRICE_SCALE_MODE = Literal["normal", "logarithmic", "percentage", "index100"]
 
 TIME = Union[datetime, pd.Timestamp, str, float]
 
 NUM = Union[float, int]
 
-FLOAT = Literal['left', 'right', 'top', 'bottom']
+FLOAT = Literal["left", "right", "top", "bottom"]
 
 
 def as_enum(value, string_types):
@@ -88,16 +95,16 @@ def as_enum(value, string_types):
 
 def marker_shape(shape: MARKER_SHAPE):
     return {
-        'arrow_up': 'arrowUp',
-        'arrow_down': 'arrowDown',
+        "arrow_up": "arrowUp",
+        "arrow_down": "arrowDown",
     }.get(shape) or shape
 
 
 def marker_position(p: MARKER_POSITION):
     return {
-        'above': 'aboveBar',
-        'below': 'belowBar',
-        'inside': 'inBar',
+        "above": "aboveBar",
+        "below": "belowBar",
+        "inside": "inBar",
     }.get(p)
 
 
@@ -126,11 +133,18 @@ class JSEmitter:
 
     def __iadd__(self, other):
         def final_wrapper(*arg):
-            other(self._chart, *arg) if not self._wrapper else self._wrapper(other, self._chart, *arg)
-        async def final_async_wrapper(*arg):
-            await other(self._chart, *arg) if not self._wrapper else await self._wrapper(other, self._chart, *arg)
+            other(self._chart, *arg) if not self._wrapper else self._wrapper(
+                other, self._chart, *arg
+            )
 
-        self._chart.win.handlers[self._name] = final_async_wrapper if asyncio.iscoroutinefunction(other) else final_wrapper
+        async def final_async_wrapper(*arg):
+            await other(
+                self._chart, *arg
+            ) if not self._wrapper else await self._wrapper(other, self._chart, *arg)
+
+        self._chart.win.handlers[self._name] = (
+            final_async_wrapper if asyncio.iscoroutinefunction(other) else final_wrapper
+        )
         self._on_iadd(other)
         return self
 
@@ -138,15 +152,19 @@ class JSEmitter:
 class Events:
     def __init__(self, chart):
         self.new_bar = Emitter()
-        self.search = JSEmitter(chart, f'search{chart.id}',
-            lambda o: chart.run_script(f'''
+        self.search = JSEmitter(
+            chart,
+            f"search{chart.id}",
+            lambda o: chart.run_script(f"""
             Lib.Handler.makeSpinner({chart.id})
             {chart.id}.search = Lib.Handler.makeSearchBox({chart.id})
-            ''')
+            """),
         )
-        salt = chart.id[chart.id.index('.')+1:]
-        self.range_change = JSEmitter(chart, f'range_change{salt}',
-            lambda o: chart.run_script(f'''
+        salt = chart.id[chart.id.index(".") + 1 :]
+        self.range_change = JSEmitter(
+            chart,
+            f"range_change{salt}",
+            lambda o: chart.run_script(f"""
             let checkLogicalRange{salt} = (logical) => {{
                 {chart.id}.chart.timeScale().unsubscribeVisibleLogicalRangeChange(checkLogicalRange{salt})
                 
@@ -156,12 +174,14 @@ class Events:
                 setTimeout(() => {chart.id}.chart.timeScale().subscribeVisibleLogicalRangeChange(checkLogicalRange{salt}), 50)
             }}
             {chart.id}.chart.timeScale().subscribeVisibleLogicalRangeChange(checkLogicalRange{salt})
-            '''),
-            wrapper=lambda o, c, *arg: o(c, *[float(a) for a in arg])
+            """),
+            wrapper=lambda o, c, *arg: o(c, *[float(a) for a in arg]),
         )
 
-        self.mouse_move = JSEmitter(chart, f'mouse_move{salt}',
-            lambda o: chart.run_script(f'''
+        self.mouse_move = JSEmitter(
+            chart,
+            f"mouse_move{salt}",
+            lambda o: chart.run_script(f"""
             let checkMouseMove = (param) => {{
                 {chart.id}.chart.unsubscribeCrosshairMove(checkMouseMove)
                 if (
@@ -184,10 +204,13 @@ class Events:
                 setTimeout(() => {chart.id}.chart.subscribeCrosshairMove(checkMouseMove{salt}), 50)
             }}
             {chart.id}.chart.subscribeCrosshairMove(checkMouseMove{salt})
-            '''),
-            wrapper=lambda o, c, *arg: o(c, *[float(a) for a in arg]))
-        self.click = JSEmitter(chart, f'click{salt}',
-            lambda o: chart.run_script(f'''
+            """),
+            wrapper=lambda o, c, *arg: o(c, *[float(a) for a in arg]),
+        )
+        self.click = JSEmitter(
+            chart,
+            f"click{salt}",
+            lambda o: chart.run_script(f"""
             let checkClick{salt} = (param) => {{
                 {chart.id}.chart.unsubscribeClick(checkClick{salt})
                 if (
@@ -212,8 +235,10 @@ class Events:
                 setTimeout(() => {chart.id}.chart.subscribeClick(checkClick{salt}), 50)
             }}
             {chart.id}.chart.subscribeClick(checkClick{salt})
-            '''),
-            wrapper=lambda o, c, *arg: o(c, *[float(a) for a in arg]))
+            """),
+            wrapper=lambda o, c, *arg: o(c, *[float(a) for a in arg]),
+        )
+
 
 class BulkRunScript:
     def __init__(self, script_func):
@@ -226,7 +251,7 @@ class BulkRunScript:
 
     def __exit__(self, *args):
         self.enabled = False
-        self.script_func('\n'.join(self.scripts))
+        self.script_func("\n".join(self.scripts))
         self.scripts = []
 
     def add_script(self, script):
